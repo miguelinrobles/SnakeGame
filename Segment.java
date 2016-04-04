9d2b7a428228442e8dbc5bcdd2a16850c0cfde87
miguel <miganrobles@gmail.com>
2016-04-04 19:20:17 +0200

    Creada clase Segment con los atributos y constructor

---
+++ Segment.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+
+/**
+ * Write a description of class Segmen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Segment
+{
+    // Punto inicial X
+    private int posicionX;
+    // Punto inicial Y
+    private int posicionY;
+    // Longitud del segmento
+    public static final int LONGITUD_SEGMENTO = 4;
+    // Direccion en la que se dibuja el segmento
+    private int direccion;
+    // Color del segmento
+    private Color color;
+
+    /**
+     * Constructor for objects of class Segmen
+     */
+    public Segment(int posicionX, int posicionY, int direccion, Color color)
+    {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.direccion = direccion;
+        this.color = color;
+    }
+}
