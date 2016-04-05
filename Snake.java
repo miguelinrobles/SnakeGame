9f0f053df17792f26c29bd5debf685171bdf4af2
miguel <miganrobles@gmail.com>
2016-04-05 10:12:47 +0200

    Creado metodo colisionSegmentos que comprueba si el segmento que queremos añadir colisiona con los segmentos existentes

--- Snake.java
+++ Snake.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Write a description of class Snake here.
@@ -19,6 +20,10 @@ public class Snake
     public static final int MUMERO_SEGMENTOS_INICIALES = 3;
     // Almacena todos los segmentos que forman la serpiente
     private ArrayList<Segment> segmentos;
+    // Diferencia de grados de una direccion a otra por ser en angulos rectos
+    private static final int DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES = 90;
+    // margenes que dejamos en el lienzo
+    private static final int MARGEN_LIENZO = 10;
 
     /**
      * Constructor for objects of class Snake
@@ -31,5 +36,18 @@ public class Snake
         segmentos = new ArrayList<>();
     }
 
-   
+    /**
+     * Comprueba si el segmento colisiona con otros segmentos
+     */
+    private boolean colisionaConOtrosSegmentos(Segment segmento)
+    {
+        boolean colisiona = false;
+        int index = 0;
+        int cantidadSegmentos = segmentos.size();
+        while (index < cantidadSegmentos && !colisiona) {
+            colisiona = segmentos.get(index).colisionaCon(segmento); 
+            index++;
+        }
+        return colisiona;
+    }
 }
